58ba5fd9d83bc49bb8141c890ee1c7a6ecdbbfac
Scott Serage <xttocsurge51@gmail.com>
2017-11-01 11:48:46 -0600

    Completed step 9

--- app/packages/packagesCtrl.js
+++ app/packages/packagesCtrl.js
@@ -1,3 +1,12 @@
-angular.module('devmtnTravel').controller('packagesCtrl', function( $scope ) {
-  
+angular.module('devmtnTravel').controller('packagesCtrl', function( $scope, $stateParams, mainSrvc ) {
+  mainSrvc.getPackageInfo().then(res => {
+
+    $scope.allPackages = res.data;
+
+    if($stateParams.country){
+      $scope.packages = $scope.allPackages.filter(package=>{
+        return package.country === $stateParams.country;
+      });
+    }
+  });
 });
