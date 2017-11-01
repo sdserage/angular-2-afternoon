5e8e9228c93ce25c9ac97a52631e57fe4b450ee2
Scott Serage <xttocsurge51@gmail.com>
2017-11-01 10:33:52 -0600

    completed step 6

--- app/mainSrvc.js
+++ app/mainSrvc.js
@@ -1,3 +1,14 @@
-angular.module('devmtnTravel').service('mainSrvc', function() {
-  
+angular.module('devmtnTravel').service('mainSrvc', function($http) {
+  this.getTravelInfo = () => {
+    return {
+      method: 'GET',
+      url: "https://practiceapi.devmountain.com/api/travel/travel-info"
+    }
+  };
+  this.getPackageInfo = () => {
+    return {
+      method: 'GET',
+      url: "https://practiceapi.devmountain.com/api/travel/package-info"
+    }
+  };
 });
