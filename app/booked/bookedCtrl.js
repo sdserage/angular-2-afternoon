ec638546b8fd25cd375c408b816ef7ca240259cb
Scott Serage <xttocsurge51@gmail.com>
2017-11-01 10:45:59 -0600

    completed step 7

--- app/booked/bookedCtrl.js
+++ app/booked/bookedCtrl.js
@@ -1,3 +1,15 @@
-angular.module('devmtnTravel').controller('bookedCtrl', function( $scope ) {
-  
+angular.module('devmtnTravel').controller('bookedCtrl', function( $scope, $stateParams, mainSrvc ) {
+  mainSrvc.getPackageInfo().then(res => {
+    $scope.allPackages = res.data;
+
+    if($stateParams.id) {
+      $scope.packageIndex = $scope.allPackages.findIndex(package => {
+        return package.id === parseInt($stateParams);
+      });
+
+      if($scope.packageIndex !== -1) {
+        $scope.package = $scope.allPackages[$scope.packageIndex];
+      }
+    }
+  });
 });
