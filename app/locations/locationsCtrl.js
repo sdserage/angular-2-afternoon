f429d0ebe625741dd13b9a2178902c9596885761
Scott Serage <xttocsurge51@gmail.com>
2017-11-01 11:21:06 -0600

    Actually completed step 8, previously completed step 7 actually

--- app/locations/locationsCtrl.js
+++ app/locations/locationsCtrl.js
@@ -1,3 +1,5 @@
-angular.module('devmtnTravel').controller('locationsCtrl', function( $scope ) {
-  
+angular.module('devmtnTravel').controller('locationsCtrl', function( $scope, mainSrvc ) {
+  mainSrvc.getTravelInfo().then(res=>{
+    $scope.locations = res.data;
+  });
 });
